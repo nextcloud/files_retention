026aab720795cea3468492a2e281c2c9577a303a
Julius Härtl <jus@bitgrid.net>
2019-12-23 11:49:13 +0100

    Add checkbox for notify setting
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -22,15 +22,33 @@
  */
 namespace OCA\Files_Retention\Settings;
 
+use OCA\Files_Retention\AppInfo\Application;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IInitialStateService;
+use OCP\IConfig;
 use OCP\Settings\ISettings;
 
 class Admin implements ISettings {
+
+	/** @var IInitialStateService */
+	protected $initialStateService;
+	/** @var IConfig */
+	protected $config;
+
+	public function __construct(IInitialStateService $initialStateService, IConfig $config) {
+		$this->initialStateService = $initialStateService;
+		$this->config = $config;
+	}
 
 	/**
 	 * @return TemplateResponse
 	 */
 	public function getForm() {
+		$this->initialStateService->provideInitialState(
+			Application::APP_ID,
+			'notify_before',
+			$this->config->getAppValue(Application::APP_ID, 'notify_before', 'no') === 'yes'
+		);
 		return new TemplateResponse('files_retention', 'admin', [], '');
 	}
 
