5f642a9704a745d34bc2b8c0fa50bd7637ecd482
sualko <klaus@jsxc.org>
2021-02-19 14:38:51 +0100

    show background job status
    if a tag gets deleted the background job is cancled without notifing
    the user. This change will show the current state on the admin page.
    
    Signed-off-by: sualko <klaus@jsxc.org>

--- js/template.js
+++ js/template.js
@@ -16,8 +16,10 @@ templates['template'] = template({"compiler":[8,">= 4.3.0"],"main":function(cont
     + alias4(((helper = (helper = lookupProperty(helpers,"timeAmount") || (depth0 != null ? lookupProperty(depth0,"timeAmount") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"timeAmount","hash":{},"data":data,"loc":{"start":{"line":3,"column":11},"end":{"line":3,"column":25}}}) : helper)))
     + "</span></td>\n	<td><span>"
     + alias4(((helper = (helper = lookupProperty(helpers,"timeUnit") || (depth0 != null ? lookupProperty(depth0,"timeUnit") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"timeUnit","hash":{},"data":data,"loc":{"start":{"line":4,"column":11},"end":{"line":4,"column":23}}}) : helper)))
+    + "</span></td>\n	<td><span>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"hasJob") || (depth0 != null ? lookupProperty(depth0,"hasJob") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"hasJob","hash":{},"data":data,"loc":{"start":{"line":5,"column":11},"end":{"line":5,"column":21}}}) : helper)))
     + "</span></td>\n	<td><a class=\"icon-delete has-tooltip\" title=\""
-    + alias4(((helper = (helper = lookupProperty(helpers,"deleteString") || (depth0 != null ? lookupProperty(depth0,"deleteString") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"deleteString","hash":{},"data":data,"loc":{"start":{"line":5,"column":47},"end":{"line":5,"column":63}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"deleteString") || (depth0 != null ? lookupProperty(depth0,"deleteString") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"deleteString","hash":{},"data":data,"loc":{"start":{"line":6,"column":47},"end":{"line":6,"column":63}}}) : helper)))
     + "\"></a></td>\n<tr>\n";
 },"useData":true});
 })();
