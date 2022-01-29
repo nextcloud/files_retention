da47d8d8f6332fcc57f33fb263f9a2330af15243
Joas Schilling <coding@schilljs.com>
2022-01-29 12:33:57 +0100

    Fix path
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- compile-handlebars-templates.sh
+++ compile-handlebars-templates.sh
@@ -3,4 +3,4 @@
 # Prefer the local handlebars script, and fall back to the global one.
 export PATH=./node_modules/.bin/:$PATH
 
-handlebars -n OCA.File_Retention.Templates js/templates/ -f js/templates.js
+handlebars -n OCA.File_Retention.Templates js/template.handlebars -f js/template.js
