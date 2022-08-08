aa0c0aa97842bc113f54928e55314c8937ccac51
Joas Schilling <coding@schilljs.com>
2022-08-08 21:22:46 +0200

    Fix eslint and stylelint configs
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,3 @@
 const stylelintConfig = require('@nextcloud/stylelint-config')
 
-stylelintConfig.ignoreFiles = ['css/At.scss']
-
-stylelintConfig.rules['at-rule-no-unknown'] = [
-	true, {
-		ignoreAtRules: ['include', 'mixin', 'use'],
-	},
-]
-
 module.exports = stylelintConfig
