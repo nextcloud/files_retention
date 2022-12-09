51d9b1b703dd980b2ad972b91c226bc5231018ad
Joas Schilling <coding@schilljs.com>
2022-12-09 22:23:12 +0100

    Remove edit method
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -26,6 +26,5 @@ return [
 		['name' => 'API#getRetentions', 'url' => '/api/v1/retentions', 'verb' => 'GET'],
 		['name' => 'API#addRetention', 'url' => '/api/v1/retentions', 'verb' => 'POST'],
 		['name' => 'API#deleteRetention', 'url' => '/api/v1/retentions/{id}', 'verb' => 'DELETE'],
-		['name' => 'API#editRetention', 'url' => '/api/v1/retentions/{id}', 'verb' => 'PUT'],
 	],
 ];
