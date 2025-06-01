1db2f91595a1020469ca280052aa809dede917ba
Joas Schilling <coding@schilljs.com>
2025-06-01 16:14:57 +0200

    ci: Update PHPUnit bootstrap
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,36 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * @copyright 2017, Roeland Jago Douma <roeland@famdouma.nl>
- *
- * @author Roeland Jago Douma <roeland@famdouma.nl>
- *
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 
-if (!class_exists('\PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-(new \OC_App())->enable('files_retention');
-\OC_App::loadApp('files_retention');
-OC_Hook::clear();
+Server::get(IAppManager::class)->enableApp('files_retention');
+Server::get(IAppManager::class)->loadApp('files_retention');
